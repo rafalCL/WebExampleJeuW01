fa709ce4f9f863fab654c641bacfa6c0338f7a5a
edu <edu@mail.pl>
2024-02-04 16:29:00 +0100

    first servlet

---
+++ src/main/java/pl/coderslab/webexamplejeuw01/examples/FirstExampleServlet.java
@@ -0,0 +1,17 @@
+package pl.coderslab.webexamplejeuw01.examples;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/firstServlet")
+public class FirstExampleServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("hello world");
+        resp.getWriter().println("hello my client");
+    }
+}
---
+++ src/main/java/pl/coderslab/webexamplejeuw01/examples/FirstExampleXmlServlet.java
@@ -0,0 +1,15 @@
+package pl.coderslab.webexamplejeuw01.examples;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FirstExampleXmlServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("superservlet");
+        resp.getWriter().println("superservlet");
+    }
+}
